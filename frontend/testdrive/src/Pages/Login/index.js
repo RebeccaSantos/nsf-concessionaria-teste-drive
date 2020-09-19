092013791c5fe61442d4d8f02f0520c890d57697
Senhor Supremo <ramonsouzasil2004@gmail.com>
2020-09-19 20:19:45 -0300

    teste2

--- frontend/testdrive/src/Pages/Login/index.js
+++ frontend/testdrive/src/Pages/Login/index.js
@@ -8,7 +8,7 @@ export default function Login(){
         <div className="Login">
 
             <div>
-                Teste
+                <input type="text">teste</input>
             </div>
 
         </div>
