8455ca87c1ce3755c95d37bed935fe0a525508c2
Senhor Supremo <ramonsouzasil2004@gmail.com>
2020-09-20 13:18:11 -0300

    indo

--- frontend/testdrive/src/Pages/Cadastrar/index.js
+++ frontend/testdrive/src/Pages/Cadastrar/index.js
@@ -1,10 +1,49 @@
 import React from "react";
+import "./style.css";
 
 export default function Cadastrar() {
 
   return (
-    <div>
-      agendar
+    <div className="Tela">
+
+      <div className="Container1">
+          
+      </div>
+
+      <div className="Container2">
+      <h1>Fazer Agendamento</h1>
+        <div className="Carross">
+          Carro   
+          <select name="Carros" id="cars">
+              <option value="Escolha seu Carro">Escolha seu Carro</option>
+              <option value="Sedan">Sedan</option>
+              <option value="volvo">Volvo</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+          </select>
+        </div> 
+
+          <br></br>
+
+          <div>
+            Dia
+            <input type="date"></input>
+          </div>
+
+          <br></br>
+
+          <div>
+            Hora
+            <input type="time"></input>
+          </div>
+          <div className="button">
+              <button type="button" class="btn btn-primary">Primary</button>
+          </div>
+      </div>
+      
+
+      
     </div>
   );
 }
--- frontend/testdrive/src/Pages/MenuCliente/index.js
+++ frontend/testdrive/src/Pages/MenuCliente/index.js
@@ -7,14 +7,23 @@ import { Link } from "react-router-dom";
 export default function MenuCliente(){
     return(
         <div className="Corpo">
-            <div className="Container-Cima">
-               <h3><Link to="/Consultar">Consultar Agendamentos</Link></h3>
 
-               <h3><Link to="/Cadastrar">Fazer Agendamentos</Link></h3>
+            <div className="Container-Cima">
+
+                <div className="agendamentos">
+                    <h3><Link to="/Consultar">Consultar Agendamentos</Link></h3>
+                </div>
+
+                <div className="cadastrar">
+                    <h3><Link to="/Cadastrar">Fazer Agendamentos</Link></h3>
+                </div>
+
             </div>
+
             <div className="Container-Baixo">
                 <h1>"Nome do Cliente"</h1>
             </div>
+
         </div>
 
        
