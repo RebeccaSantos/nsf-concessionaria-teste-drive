74b128d50a1a636dde5e220ea44bd6f0d1b9cc75
João Victor Mendes <vitormendes122016@gmail.com>
2020-09-19 22:22:19 -0300

    services pronta(euacho)

--- frontend/testdrive/src/Service/TestDriveApi.js
+++ frontend/testdrive/src/Service/TestDriveApi.js
@@ -1,28 +1,32 @@
 import axios from 'axios';
+const api = axios.create({
+    baseURL: "http://localhost:5000"
+  });
 
 
 export default class TestDriveApi{
-        async cadastrar(lf){
-            const resp = await api.post('/testdrive', lf);
+
+        async login(req){
+            const resp = await api.post('/login', req);
             return resp;
         }
 
-        async consultar() {
-            const resp = await api.get('/testdrive');
+        async consultar(id) {
+            const resp = await api.get(`/Cliente/Consultar/${id}`);
             console.log(resp.data)
           
             return resp.data;
         }
 
-        async alterar(id,lf){
-            const resp = await api.put('/testdrive/' + id, lf);
-             return resp;
-        }
-
-        async excluir(id) {
-            const resp = await api.delete('/testdrive/' + id);
+        async agendar(id,req) {
+            const resp = await api.post(`/cliente/${id}`,req);
           
             return resp.data;
+        }
+
+        async feedback(id,req){
+            const resp = await api.put(`/feedback/${id}`, req);
+             return resp;
         }
     
 }
