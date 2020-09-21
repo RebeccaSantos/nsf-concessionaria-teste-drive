04e38f29652f613a737b42de0144ec55bc568bd0
infob29 <infob29@edu.frei>
2020-09-21 10:53:20 -0300

    Arrumando Business

--- backend/Business/TestDriveBusiness.cs
+++ backend/Business/TestDriveBusiness.cs
@@ -10,76 +10,50 @@ namespace backend.Business
     public class TestDriveBusiness
     {
         Database.TestDriveDatabase database=new Database.TestDriveDatabase();
+        Validar.ValidarTestDrive validar=new Validar.ValidarTestDrive();
         public Models.TbLogin VerificarLogin(Models.Request.TestDriveRequest.Login req )
         {
             Models.TbLogin tabela=database.verificarLogin(req);
-            
-            if(tabela==null)
-                  throw new ArgumentException("username ou senha incorretos");
-
-                  return tabela;
-
+            validar.VerificarLogin(tabela);
+            return tabela;
         }
         public string VerificarPerfil (Models.TbLogin tabela)
         {
             return database.VerificarPerfil(tabela);
         }
-    
         public Models.TbCliente verificarCliente(Models.TbLogin tb)
         {
                 return database.verificarCliente(tb);
-          
         }
       public Models.TbFuncionario verificarFucionario(Models.TbLogin tb)
         {
                 return database.verificarFuncionario(tb);
-          
         }
         public List<Models.TbAgendamento> AgendamentosCliente(int id)
         {
-                if(id<=0)
-                   throw new ArgumentException("id invalido");
+             validar.Validacao(id);
 
             return database.Agendamentos(id);
         }
         public Models.TbAgendamento ValidarAgendamento(Models.TbAgendamento ag)
         {
-                if(ag.IdCliente<=0)
-                    throw new ArgumentException("id invalido");
-
-                if(ag.IdFuncionario<=0)
-                    throw new ArgumentException("id invalido");
-
-                if(ag.DtAgendamento==new DateTime()||ag.DtAgendamento<=DateTime.Now)
-                throw new ArgumentException("Essa Data ja passou");
-
-                if(ag.DtAgendamento.Value.DayOfWeek==DayOfWeek.Saturday||
-                ag.DtAgendamento.Value.DayOfWeek==DayOfWeek.Sunday)
-                        throw new  ArgumentException("Não atendemos aos finais de semana");
-
-
-
-                return database.Agendamento(ag);
+              validar.ValidarAgendamento(ag);
+               return database.Agendamento(ag);
         }
-                
-             
        public Models.TbAgendamento ValidarFeedback(Models.Request.TestDriveRequest.RequestFeedback req ,int id)
         {
-            if(req.Feedback<0)
-                throw new ArgumentException("Feedback invalido");
-                
+            
+                validar.ValidarFeedBack(req.Feedback,id);    
                 return database.Feedback(req,id);
 
         }
         public Models.TbCarro Verificarcarro(string carro)
         {
-            if(string.IsNullOrEmpty(carro))
-                throw new ArgumentException("carro é obrigatorio");
+               validar.VerificarCarro(carro);
                 
               return database.VerificarCarro(carro);
         }
-
-        public List<Models.TbCarro> ListarCarros()
+       public List<Models.TbCarro> ListarCarros()
         {
             List<Models.TbCarro> carros=database.ListarCarros();
 
