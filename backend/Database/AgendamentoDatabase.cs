5d696ae265943c2bd2408437d2e942fc261dfeb0
infob29 <infob29@edu.frei>
2020-09-28 09:45:30 -0300

    Joao feio

--- backend/Database/AgendamentoDatabase.cs
+++ backend/Database/AgendamentoDatabase.cs
@@ -12,7 +12,7 @@ namespace backend.Database
             List<Models.TbAgendamento> agendamento = ctx.TbAgendamento.Include(x => x.IdClienteNavigation)
                                                                      .Include(x => x.IdFuncionarioNavigation)
                                                                      .Include(x => x.IdCarroNavigation)
-                                                                     .Where(x => x.IdClienteNavigation.IdLogin == id).ToList();
+                                                                     .Where(x => x.IdClienteNavigation.IdLoginNavigation.IdLogin == id).ToList();
             return agendamento;
         }
         public Models.TbAgendamento SalvarAgendamento(Models.TbAgendamento ag)
